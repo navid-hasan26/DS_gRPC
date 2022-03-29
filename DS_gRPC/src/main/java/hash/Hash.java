86e9fdcd89fa3701004ec485700fa8b872a7c2f8
Navid Hasan <53113814+navid-hasan26@users.noreply.github.com>
2022-03-29 13:16:20 +0600

    added hash and proto file

--- DS_gRPC/src/main/java/hash/Hash.java
+++ DS_gRPC/src/main/java/hash/Hash.java
@@ -1,4 +1,23 @@
 package hash;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+
 public class Hash {
+    // use sha256
+    public static String getHashed(String str) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(str.getBytes(StandardCharsets.UTF_8));
+            StringBuilder hexString = new StringBuilder();
+            for (int i = 0; i < hash.length; i++) {
+                String hex = Integer.toHexString(0xff & hash[i]);
+                if (hex.length() == 1) hexString.append('0');
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
